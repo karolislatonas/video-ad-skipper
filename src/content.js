aadb573bb713ee11583661ea7aa48e4e97b1a748
DESKTOP-RKPI6VD\KarolisL <karolis.latonas@gmail.com>
2021-06-30 19:33:03 +0300

    Skip video player which is visible

--- src/content.js
+++ src/content.js
@@ -7,23 +7,38 @@ class AdSkipper{
     SkipAd(){
         this.video = this.FindVideoElement();
         
+        if(!this.video){
+            return;
+        }
+            
         this.video.onloadedmetadata = this.OnVideoChange.bind(this);
         
         this.OnVideoChange();
     }
 
     OnVideoChange(){
-        this.video.currentTime = this.video.duration + 1;
+        if(Number.isNaN(this.video.duration)){
+            this.video.src="https://play.tv3.lt/3b70be0a-457c-45e9-8178-f2013a7c51b7"
+            console.dir(this.video);
+            console.log(this.video.src);
+            
+        }
+        else{
+            this.video.currentTime = this.video.duration + 1;
+        }
     }
     
     FindVideoElement(){
         var videos = document.getElementsByTagName('video');
-    
+        
         if(videos.length === 0){
             videos = this.FindVideoElementInIFrames();
         }
+
+        console.log(videos);
     
-        return videos[0];
+        return [...videos]
+            .find(v => this.IsElementVisible(v) && !Number.isNaN(v.duration));
     }
 
     FindVideoElementInIFrames(){
@@ -39,6 +54,15 @@ class AdSkipper{
             .filter(v => v.length > 0)
 
         return videos[0];
+    }
+    
+    IsElementVisible(element){
+        var rect = element.getBoundingClientRect();
+        var elemTop = rect.top;
+        var elemBottom = rect.bottom;
+
+        var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+        return isVisible;
     }
 }
 
